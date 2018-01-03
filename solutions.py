f6e9cab580aaf4cc079a3e90fd874ec7fc1b3dd0
Jose Ramirez <jdevla@protonmail.com>
2018-01-02 20:20:15 -0800

    fix: added comments and more test cases

--- solutions.py
+++ solutions.py
@@ -1,15 +1,34 @@
 def anagram(s1, s2):
+    #converting both strings into lists
     s1 = list(s1)
     s2 = list(s2)
-    s1.sort()
-    s2.sort()
+    #sorting the elements of the list
     return sorted(s1) == sorted(s2)
 
 def Question1(t, s):
+    #gets length of strings 
     t_len = len(t)
     s_len = len(s)
     t_sort = sorted(t)
     return any(anagram(s[i: i+len(t)], t)
                  for i in range(len(s)-len(t)+ 1))
 
+# Test case 1 checking for anagram in subject
 print Question1("ad", "udacity")
+#True
+
+# Test case 2 using words not in subject
+print Question1("qwr", "udacity")
+#False
+
+# Test case 3 using a word longer than the subject
+print Question1("cityadunreal", "udacity")
+#False
+
+# Test case 4 using words in subject but not in order
+print Question1("uiy", "udacity")
+#False
+
+# Test case 5 with empty string
+print Question1("", "udacity")
+#True
